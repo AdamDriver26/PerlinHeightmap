43080a0e2d59ba63870107dcff8afcc9f9574378
AdamDriver26 <adamjdriver26@gmail.com>
2020-08-12 16:24:30 +0100

    Added the ability to read and write from/to a config file.

--- PerlinHeightmap/src/Config.java
+++ PerlinHeightmap/src/Config.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -6,6 +11,54 @@
  * @author Adam
  *
  */
+
 public class Config {
-	
+	// the name of the resulting image
+	String name;
+	// The map dimensions as an array
+	int[] dim;
+	// Sea level
+	int seaLevel;
+
+	Config(String n, int[] d, int s) {
+		name = n;
+		dim = d;
+		seaLevel = s;
+	}
+
+	public static void write() throws Exception {
+
+		String defaultName = "heightmap";
+		int[] defaultDim = { 100, 100 };
+		int defaultSeaLevel = 20;
+		Config defaultConfig = new Config(defaultName, defaultDim, defaultSeaLevel);
+
+		File param = new File("config.txt");
+		PrintStream p = new PrintStream(param);
+
+		p.println("Filename: " + defaultConfig.name);
+		p.println("Dimensions: " + defaultConfig.dim[0] + "," + defaultConfig.dim[1]);
+		p.println("Sea level: " + defaultConfig.seaLevel);
+
+		p.close();
+
+	}
+
+	public static Config read() throws Exception {
+
+		File param = new File("config.txt");
+
+		Scanner s = new Scanner(param);
+
+		String name = s.nextLine().substring(10);
+		String[] stringDim = s.nextLine().substring(12).split(","); // Reads 
+		int[] dim = {Integer.parseInt(stringDim[0]),Integer.parseInt(stringDim[1])};
+		int seaLevel = Integer.valueOf(s.nextLine().substring(11));
+
+		s.close();
+
+		return new Config(name, dim, seaLevel);
+
+	}
+
 }
--- PerlinHeightmap/src/Main.java
+++ PerlinHeightmap/src/Main.java
@@ -1,7 +1,9 @@
+
 /**
  * @author Adam Driver
  *
  */
+
 public class Main {
 
 	/**
@@ -9,7 +11,20 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//		try {
+//			Config.write();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		Config param = null;
+		try {
+			param = Config.read();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(param.name + ",[" + param.dim[0] + "," + param.dim[1] + "]," + param.seaLevel);
 	}
 
 }
