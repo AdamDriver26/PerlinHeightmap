6b66e37d9050c377d7d6d1f903c7b1d5fffcab20
AdamDriver26 <adamjdriver26@gmail.com>
2020-11-10 23:12:40 +0000

    Better Grass Implementation
    Grass is only shaded on sufficiently flat areas. Similar condition is applied to sand but the additional condition of being within 5% of seaLevel is present. The ability of grass to grow is determined by the grassThreshold variable.
    
    Implemented primarily in DrawHeights class.
    
    Currently colours of sand and other non-grass land needs to be fixed.
    Sand often has lines of grass within it. Erosion would likely fix this particular issue.

--- PerlinHeightmap/src/Config.java
+++ PerlinHeightmap/src/Config.java
@@ -12,23 +12,26 @@ import java.util.Scanner;
  */
 
 public class Config {
-	// The name of the resulting image
+	// The name of the resulting image.
 	String name;
-	// The map dimensions as an array
+	// The map dimensions as an array.
 	int[] dim;
-	// Where to draw sea level
+	// Where to draw sea level.
 	int seaLevel;
-	// The size of each square of the grid used to create the Perlin noise
+	// The size of each square of the grid used to create the Perlin noise.
 	int scale;
-	//
+	// The style of the base layer. Current options are 'plane','island','archipelago'.
 	String style;
+	// Controls the angle at which grass will grow.
+	int grassThreshold;
 
-	Config(String n, int[] D, int l, int sc, String st) {
+	Config(String n, int[] D, int l, int sc, String st, int gt) {
 		name = n;
 		dim = D;
 		seaLevel = l;
 		scale = sc;
 		style = st;
+		grassThreshold = gt;
 		
 	}
 
@@ -39,8 +42,9 @@ public class Config {
 		int defaultSeaLevel = 110;
 		int defaultScale = 115;
 		String defaultStyle = "island";
+		int defaultGrassThreshold = 24;
 				
-		Config defaultConfig = new Config(defaultName, defaultDim, defaultSeaLevel, defaultScale, defaultStyle);
+		Config defaultConfig = new Config(defaultName, defaultDim, defaultSeaLevel, defaultScale, defaultStyle, defaultGrassThreshold);
 
 		File param = new File("config.txt");
 		PrintStream p = new PrintStream(param);
@@ -50,6 +54,7 @@ public class Config {
 		p.println("Sea level: " + defaultConfig.seaLevel);
 		p.println("Noise scale: " + defaultConfig.scale);
 		p.println("Map style: " + defaultConfig.style);
+		p.println("Grass growth threshold: " + defaultConfig.grassThreshold);
 
 		p.close();
 
@@ -62,15 +67,16 @@ public class Config {
 		Scanner s = new Scanner(param);
 
 		String name = s.nextLine().substring(10);
-		String[] stringDim = s.nextLine().substring(12).split(","); // Reads 
+		String[] stringDim = s.nextLine().substring(12).split(",");
 		int[] dim = {Integer.parseInt(stringDim[0]),Integer.parseInt(stringDim[1])};
 		int seaLevel = Integer.valueOf(s.nextLine().substring(11));
 		int scale = Integer.valueOf(s.nextLine().substring(13));
 		String style = s.nextLine().substring(11);
+		int grassThreshold = Integer.valueOf(s.nextLine().substring(24));
 
 		s.close();
 
-		return new Config(name, dim, seaLevel, scale, style);
+		return new Config(name, dim, seaLevel, scale, style, grassThreshold);
 
 	}
 
--- PerlinHeightmap/src/DrawHeights.java
+++ PerlinHeightmap/src/DrawHeights.java
@@ -23,9 +23,20 @@ public class DrawHeights {
 					g2d.setColor(new Color(0, blue/2, blue));
 				}
 				
-				else {
+				else if (steepnessCheck(param, map, x, y) < 3*param.grassThreshold/4 && map[x][y] < 1.05*param.seaLevel) {
+					int green = 204*(map[x][y] - param.seaLevel)/(255-param.seaLevel) + 51;
+					g2d.setColor(new Color(194, 178, 128));
+				}
+				
+				else if (steepnessCheck(param, map, x, y) < param.grassThreshold) {
 					int green = 204*(map[x][y] - param.seaLevel)/(255-param.seaLevel) + 51;
 					g2d.setColor(new Color(green/4, green, green/4));
+				}
+				
+				else {
+					int brown = 204*(map[x][y] - param.seaLevel)/(255-param.seaLevel) + 51;
+					//g2d.setColor(new Color(brown, ( (int) 0.76*brown ), ( (int) 0.53*brown )));
+					g2d.setColor(new Color(brown, brown, brown));
 				}
 				
 				g2d.drawLine(x, y, x, y);
@@ -40,6 +51,63 @@ public class DrawHeights {
 			e.printStackTrace();
 		}
 
+	}
+	
+	private static double steepnessCheck(Config param, int[][] map, int x, int y) {
+		double angleTotal = 0.0;
+		int count = 0;
+		boolean result = false;
+		
+		// North
+		if (y > 0) {
+			angleTotal += Math.atan( Math.abs(map[x][y - 1] - map[x][y]) );
+			count++;
+		}
+		
+		// North East
+		if ( (y > 0) && (x < param.dim[0] - 1) ) {
+			angleTotal += Math.atan( Math.abs(map[x + 1][y - 1] - map[x][y]) / 1.414 );
+			count++;
+		}
+		
+		// East
+		if (x < param.dim[0] - 1) {
+			angleTotal += Math.atan( Math.abs(map[x + 1][y] - map[x][y]) );
+			count++;
+		}
+		
+		// South East
+		if ( (x < param.dim[0] - 1) && (y < param.dim[1] - 1) ) {
+			angleTotal += Math.atan( Math.abs(map[x + 1][y + 1] - map[x][y]) / 1.414);
+			count++;
+		}
+		
+		// South
+		if (y < param.dim[1] - 1) {
+			angleTotal += Math.atan( Math.abs(map[x][y + 1] - map[x][y]) );
+			count++;
+		}
+		
+		// South West
+		if ( (x > 0) && (y < param.dim[1] - 1) ) {
+			angleTotal += Math.atan( Math.abs(map[x - 1][y + 1] - map[x][y]) / 1.414);
+			count++;
+		}
+		
+		// West
+		if (x > 0) {
+			angleTotal += Math.atan( Math.abs(map[x - 1][y] - map[x][y]) );
+			count++;
+		}
+		
+		// North West
+		if ( (x > 0) && (y > 0) ) {
+			angleTotal += Math.atan( Math.abs(map[x - 1][y - 1] - map[x][y]) / 1.414);
+			count++;
+		}
+		
+		// Scales the average angle of all counted neighbours to degrees.
+		return (180.0/3.14)*angleTotal/count;
 	}
 
 }
